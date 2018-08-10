5a82aa0ae6aa1226247d73c7cbeb1124afaa8cc8
estrangegit <etienne.estrangin@gmail.com>
2018-08-11 01:06:45 +0200

    Inversion de contrôle avec Spring: annotations => contexte XML (correction AbstractResource => abstract class)

--- ticket-webapp/src/main/java/org/example/demo/ticket/webapp/rest/resource/AbstractResource.java
+++ ticket-webapp/src/main/java/org/example/demo/ticket/webapp/rest/resource/AbstractResource.java
@@ -2,7 +2,7 @@ package org.example.demo.ticket.webapp.rest.resource;
 
 import org.example.demo.ticket.business.contract.ManagerFactory;
 
-public class AbstractResource {
+public abstract class AbstractResource {
 
     private static ManagerFactory managerFactory;
 
